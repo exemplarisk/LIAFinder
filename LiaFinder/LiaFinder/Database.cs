4a6c39ff460d4bddec6cf9c61d9d31981caa2eca
Jonatan Schultz <jonatan_schultz@hotmail.com>
2020-09-25 12:57:06 +0200

    initial commit

---
+++ LiaFinder/LiaFinder/Database.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace LiaFinder
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Student>().Wait();
+        }
+        public Task<List<Student>> GetStudentAsync()
+        {
+            return _database.Table<Student>().ToListAsync();
+        }
+        public Task<int> SaveStudentAsync(Student student)
+        {
+            return _database.InsertAsync(student);
+        }
+    }
+}
---
+++ LiaFinder/LiaFinder/Student.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace LiaFinder
+{
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentEmail { get; set; }
+        public int Age { get; set; }
+    }
+}
