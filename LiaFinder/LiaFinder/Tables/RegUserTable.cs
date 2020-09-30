f6da15992120da6266b60f02312602fa92e4e222
Jonatan Schultz <jonatan_schultz@hotmail.com>
2020-09-30 13:44:19 +0200

    RegisterUser

---
+++ LiaFinder/LiaFinder/Tables/RegUserTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LiaFinder.Tables
+{
+    public class RegUserTable
+    {
+  
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ LiaFinder/LiaFinder/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using SQLite;
+using LiaFinder.Tables;
+
+namespace LiaFinder.Views
+{
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "student.db3");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text
+            };
+
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulations", "User Registration Successful", "Yes", "Cancel");
+            });
+                
+        }
+    }
+}
