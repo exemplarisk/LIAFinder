794a40d30ac13ab048858d7c42033fa25c9b9a66
zphinxify <benjamiiiin94rgmail.com>
2020-09-30 12:48:29 +0200

    Login page first commit

---
+++ LiaFinder/LiaFinder/Views/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiaFinder.Shared.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LiaFinder.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            
+            if(user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Successful", "Okay");
+            }
+            else
+            DisplayAlert("Error", "Login not correct, empty username or password","Return");
+        }
+    }
+}
