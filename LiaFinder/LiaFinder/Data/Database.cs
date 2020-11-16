a9c8b7f6f0258218068b2a0def3f48954e4f350c
Benjamin Ben Taleb <bbt@toxic.se>
2020-11-16 10:03:40 +0100

    (Updated) tuned searchefunction, is now nullchecked

--- LiaFinder/LiaFinder/Data/Database.cs
+++ LiaFinder/LiaFinder/Data/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using LiaFinder.Models;
@@ -66,13 +67,29 @@ namespace LiaFinder
             Db.Insert(user);
         }
 
-        //TODO: Implement feature to search based on Ad-Title
         public static List<Ad> SearchAd(string query)
         {
-            var search = Db.Table<Ad>().Where(a => a.AdTitle.Contains(query) 
-                                                || a.CompanyName.Contains(query)
-                                                || a.AdSkills.Contains(query)).ToList();
-            return search;
+
+            List<Ad> matches = new List<Ad>();
+
+            query = query.ToUpper();
+            var ads = Db.Table<Ad>()?.ToList();
+
+            if(ads != null)
+            {
+                foreach (var ad in ads)
+                {
+                    var titleMatches = ad.AdTitle?.ToUpper()?.Contains(query) ?? false;
+                    var companyNameMatches = ad.CompanyName?.ToUpper()?.Contains(query) ?? false;
+                    var AdSkillsMatches = ad.AdSkills?.ToUpper()?.Contains(query) ?? false;
+
+                    if (titleMatches || companyNameMatches || AdSkillsMatches)
+                    {
+                        matches.Add(ad);
+                    }
+                }
+            }
+            return matches;
         }
 
         public static bool IsUserAlreadyRegistered(string userName)
